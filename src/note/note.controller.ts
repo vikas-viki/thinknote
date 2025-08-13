b669fef8bd26901e863b286963b29da765c3001c
vikas-viki <viki57394@gmail.com>
2025-08-13 11:30:10 +0530

    basic notes done

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   Patch,
   Post,
+  Request,
   UseGuards,
 } from '@nestjs/common';
 import { NoteDataDTO, UpdateNoteDataDTO } from './dtos/note.dto';
 import { NoteService } from './note.service';
 import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
+import type { Request as HttpRequest } from 'express';
 
 @Controller('notes')
 @UseGuards(JwtAuthGuard)
@@ -18,18 +20,13 @@ export class NoteController {
   constructor(private noteService: NoteService) { }
 
   @Get()
-  getAllNotes() {
-    return this.noteService.getAllNotes();
+  getAllNotes(@Request() req: HttpRequest) {
+    return this.noteService.getAllNotes(req);
   }
 
   @Post()
-  addNewNote(@Body() data: NoteDataDTO) {
-    return this.noteService.addNewNote(data);
-  }
-
-  @Get(':id')
-  getNote(@Param('id') id: string) {
-    return this.noteService.getNote(id);
+  addNewNote(@Body() data: NoteDataDTO, @Request() req: HttpRequest) {
+    return this.noteService.addNewNote(data, req);
   }
 
   @Delete(':id')
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,27 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { NoteDataDTO, UpdateNoteDataDTO } from './dtos/note.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
 
 @Injectable()
 export class NoteService {
-  constructor(private prisma: PrismaService) {}
-  getAllNotes() {
-    return 'these are your notes';
+  constructor(private prisma: PrismaService) { }
+  getAllNotes(req: Request) {
+    return req.user!.notes;
   }
 
-  addNewNote(data: NoteDataDTO) {
-    return 'new note added';
+  async addNewNote(data: NoteDataDTO, req: Request) {
+    const note = await this.prisma.notes.create({
+      data: {
+        note: data.note,
+        title: data.title,
+        userId: req.user!.id
+      }
+    });
+    return { message: 'New note added successfully!', noteId: note.id };
   }
 
-  getNote(id: string) {
-    return 'this is your new note';
+  async deleteNote(id: string) {
+    await this.prisma.notes.delete({
+      where: {
+        id
+      }
+    });
+    return 'The note has been deleted';
   }
 
-  deleteNote(id: string) {
-    return 'the note has been deleted';
-  }
-
-  updateNote(data: UpdateNoteDataDTO) {
+  async updateNote(data: UpdateNoteDataDTO) {
+    await this.prisma.notes.update({
+      where: {
+        id: data.id
+      },
+      data: {
+        note: data.note,
+        title: data.title
+      }
+    })
     return 'note data update';
   }
 }
